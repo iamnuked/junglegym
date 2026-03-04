1e3e3121c1241795253d46d3b789131c7c487668
binny <binny0x00@gmail.com>
2026-03-05 01:49:31 +0900

    사용자 데이터 조회, 회원 정보 수정 api 추가

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import *
 import os
 from dotenv import load_dotenv
 from werkzeug.security import check_password_hash
-
+from datetime import timedelta
 
 load_dotenv()
 
@@ -19,8 +19,10 @@ user_collection = db['user']
 
 
 app = Flask(__name__)
+# 우선 access token만 사용, 유효기간 2시간 설정 -> 추후 가능하면 refresh token 구현
 app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
-
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+jwt = JWTManager(app)
 
 @app.route('/')
 def home():
@@ -75,7 +77,57 @@ def delete():
     db.USER.delete_one({"id": current_user})
     return jsonify({'result':'회원 탈퇴 성공'})
 
+# 회원 가입 수정 페이지 진입 시 필요한 데이터 전달
+@app.route("/me", methods=["GET"])
+@jwt_required()
+def get_me():
+    current_user_id = get_jwt_identity()
+    current_user = user_collection.find_one({"id":current_user_id})
 
+    return jsonify(current_user)
+
+@app.route("/profile-edit", methods=["POST"])
+@jwt_required()
+def edit():
+    current_user_id = get_jwt_identity()
+
+    # 수정 가능한 데이터 : 이름, 비밀번호, 기수, 번호
+    name_receive = request.form.get("name_give")
+    pw_receive = request.form.get("pw_give")
+    gen_receive = request.form.get("gen_give")
+    number_receive = request.form.get("number_give")
+
+    user = user_collection.find_one({"id": current_user_id})
+
+    if not user:
+        return jsonify(success=False, message="사용자를 찾을 수 없음")
+    
+    edit_data = {}
+
+    if name_receive:
+        edit_data["name"] = name_receive
+
+    if pw_receive:
+        edit_data["pw"] = pw_receive
+
+    if gen_receive:
+        edit_data["gen"] = gen_receive
+
+    if number_receive:
+        edit_data["number"] = number_receive
+
+    if edit_data:
+        result = user_collection.update_one(
+            {"id": current_user_id},
+            {"$set": edit_data}
+        )
+
+        # 실제로 변경된 문서 순
+        if result.modified_count > 0:
+            return jsonify(success=True)
+        else:
+            # case를 나누는게 조금 애매해서 우선은 False 반환
+            return jsonify(success=False)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=os.environ.get("PORT"), debug=True)
