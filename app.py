48f2fb7a221a058595116b03bf3c5a55c03c4164
iamnuked <qjawls6262@gmail.com>
2026-03-05 00:21:27 +0900

    회원가입 작성

--- app.py
+++ app.py
@@ -33,5 +33,23 @@ def login():
     # 로그인 정보 맞으면  return {"access_token": access_token}
 
 
+@app.route("/join", methods=["POST"])
+def join():
+    id_receive = request.form["id_give"]
+    pw_receive = request.form["pw_give"]
+    name_receive = request.form["pw_give"]
+    gen_receive = request.form["pw_give"]
+    number_receive = request.form["pw_give"]
+
+    # 중복 아이디 검사 조작 검사용
+    if db.USER.count_documents({"id": id_receive}) > 0:
+        return jsonify({'result': '중복된 아이디입니다.'})
+    
+    user = {'id': id_receive, 'pw': pw_receive, 'name': name_receive, 'gen': gen_receive, 'number_receive': number_receive}
+    db.USER.insert_one(user)
+    return jsonify({'result':'회원가입 성공'})
+
+
+
 if __name__ == __name__:
     app.run('0.0.0.0', port=5001, debug=True)
