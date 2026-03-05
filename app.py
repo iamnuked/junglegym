5aebc918b6f076cc063f9ba5d981cf883a5116d8
iamnuked <qjawls6262@gmail.com>
2026-03-05 20:52:31 +0900

    혼잡도 데이터 전송 api 작성

--- app.py
+++ app.py
@@ -11,6 +11,35 @@ import time
 load_dotenv()
 
 
+
+#####################################################################################
+# 이 부분은 코드를 건드리지 말고 그냥 두세요. 코드를 이해하지 못해도 상관없는 부분입니다.
+#
+# ObjectId 타입으로 되어있는 _id 필드는 Flask 의 jsonify 호출시 문제가 된다.
+# 이를 처리하기 위해서 기본 JsonEncoder 가 아닌 custom encoder 를 사용한다.
+# Custom encoder 는 다른 부분은 모두 기본 encoder 에 동작을 위임하고 ObjectId 타입만 직접 처리한다.
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+
+# 위에 정의되 custom encoder 를 사용하게끔 설정한다.
+app.json = CustomJSONProvider(app)
+
+# 여기까지 이해 못해도 그냥 넘어갈 코드입니다.
+# #####################################################################################
+
+
 # client = MongoClient('mongodb://admin:admin@10.0.134.192', 27017)
 client = MongoClient(os.environ.get("MONGO_URI"), 27017)
 db = client["junglegym"]
@@ -331,8 +360,21 @@ def get_history():
 
 # 혼잡도 데이터 전송 관련
 
-# @app.route("/now_complex")
-#     gym_data_collection.
+@app.route("/get_now_complex", methods=["GET"])
+def get_now_complex():
+    now_complex_data = gym_data_collection.find({"datetime": "now"})
+    return jsonify(now_complex_data)
+
+@app.route("/get_today_complex", methods=["GET"])
+def get_totad_complex():
+    start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    end = start + timedelta(days=1)
+    today_complex_data = gym_data_collection.find({"datetime": {
+        "$gte": start,
+        "$lt": end
+    }})
+    return jsonify(today_complex_data)
+
 
 
 ######################
